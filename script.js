30f70dae96d6854e0fa6cb488313de186bd386bb
Santosh <santykr7@gmail.com>
2023-08-14 00:53:36 +0530

    error validation

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function displayInput() {
-  const email = document.getElementById("email").value;
-  localStorage.setItem("ls_email", email);
-}
-//   const mobile = document.getElementById("mobile").value;
 
-//   form.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//   });
-
-//   localStorage.setItem("ls_email", email);
-//   localStorage.setItem("ls_mobile", mobile);
-// }
-// function setError(ele, errorMsg) {
-//   const parent = ele.parentElement;
-// }
